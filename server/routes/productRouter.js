cbf04cd6c1884760fc52bfaaf298e141b313f5a9
Юлия <blizzardoutside@yandex.ru>
2025-06-17 23:49:41 +0700

    Update productRouter.js

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,9 +1,9 @@
 const Router = require('express')
 const router = new Router()
-const deviceController = require('../controllers/deviceController')
+const productController = require('../controllers/productController')
 
-router.post('/', deviceController.create)
-router.get('/', deviceController.getAll)
-router.get('/:id', deviceController.getOne)
+router.post('/', productController.create)
+router.get('/', productController.getAll)
+router.get('/:id', productController.getOne)
 
 module.exports = router
