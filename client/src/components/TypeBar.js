335e1737c7cb71a455714c2b2023f6fd78751172
Юлия <blizzardoutside@yandex.ru>
2025-06-17 22:50:41 +0700

    Update TypeBar.js

--- client/src/components/TypeBar.js
+++ client/src/components/TypeBar.js
@@ -5,14 +5,14 @@ import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const TypeBar = observer(() => {
-    const {device} = useContext(Context)
+    const {product} = useContext(Context)
     return (
         <ListGroup>
-            {device.types.map(type =>
+            {product.types.map(type =>
                 <ListGroup.Item
                     style={{cursor: 'pointer'}}
-                    active={type.id === device.selectedType.id}
-                    onClick={() => device.setSelectedType(type)}
+                    active={type.id === product.selectedType.id}
+                    onClick={() => product.setSelectedType(type)}
                     key={type.id}
                 >
                     {type.name}
