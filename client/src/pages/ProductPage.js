b21be93b6436a4f977d1ca415f0402fa7299e7a6
Юлия <blizzardoutside@yandex.ru>
2025-06-17 23:02:01 +0700

    Update ProductPage.js

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -2,13 +2,13 @@ import React, {useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import bigStar from '../assets/bigStar.png'
 import {useParams} from 'react-router-dom'
-import {fetchOneDevice} from "../http/deviceAPI";
+import {fetchOneDevice} from "../http/productAPI";
 
-const DevicePage = () => {
-    const [device, setDevice] = useState({info: []})
+const ProductPage = () => {
+    const [product, setProduct] = useState({info: []})
     const {id} = useParams()
     useEffect(() => {
-        fetchOneDevice(id).then(data => setDevice(data))
+        fetchOneProduct(id).then(data => setProduct(data))
     }, [])
 
     return (
@@ -19,13 +19,7 @@ const DevicePage = () => {
                 </Col>
                 <Col md={4}>
                     <Row className="d-flex flex-column align-items-center">
-                        <h2>{device.name}</h2>
-                        <div
-                            className="d-flex align-items-center justify-content-center"
-                            style={{background: `url(${bigStar}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
-                        >
-                            {device.rating}
-                        </div>
+                        <h2>{Product.name}</h2>                        
                     </Row>
                 </Col>
                 <Col md={4}>
@@ -33,15 +27,15 @@ const DevicePage = () => {
                         className="d-flex flex-column align-items-center justify-content-around"
                         style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
                     >
-                        <h3>От: {device.price} руб.</h3>
+                        <h3>От: {product.price} руб.</h3>
                         <Button variant={"outline-dark"}>Добавить в корзину</Button>
                     </Card>
                 </Col>
             </Row>
             <Row className="d-flex flex-column m-3">
                 <h1>Характеристики</h1>
-                {device.info.map((info, index) =>
-                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                {product.info.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgreen' : 'transparent', padding: 10}}>
                         {info.title}: {info.description}
                     </Row>
                 )}
