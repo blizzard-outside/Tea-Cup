77e677e92e0771c6955d72a1953cae5bc0170ab2
Юлия <blizzardoutside@yandex.ru>
2025-06-17 23:50:57 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ const typeRouter = require('./typeRouter')
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
-router.use('/brand', varietyRouter)
-router.use('/device', productRouter)
+router.use('/variety', varietyRouter)
+router.use('/product', productRouter)
 
 module.exports = router
